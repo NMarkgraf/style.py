c1b40c44000b7ede19db8f8b4cc74060dd4be276
Norman Markgraf <nmarkgraf@hotmail.com>
2018-06-27 12:47:39 +0200

    Kleinere Updates. Code weiter dokumentiert. Erste Schritte hin zu einer neuen Funktion SPAN Blöcke für Indizes vorbereitet.

--- decorator.py
+++ decorator.py
@@ -11,6 +11,7 @@
   0.1   - 05.04.2018 (nm) - Erste Version
   0.2   - 27.03.2018 (nm) - Code (angeblich) "wartbarer" gemacht.
   0.2.1 - 14.06.2018 (nm) - Code noch "wartbarer" gemacht. ;-)
+  0.2.2 - 27.06.2018 (nm) - Code etwas mehr kommentiert.
 
 
   WICHTIG:
@@ -61,35 +62,93 @@ class Decorator:
         "italic", "smallcaps", "slanted", "upright")
 
     def __init__(self):
+        """
+
+        """
         self.pre = ""
         self.post = ""
 
     def addPrePost(self, prepost=("", "")):
+        """
+
+        :param prepost:
+        :return:
+        """
         self.addPre(prepost[0])
         self.addPost(prepost[1])
 
     def addPost(self, post):
+        """
+
+        :param post:
+        :return:
+        """
         self.post = post + self.post
 
     def addPre(self, pre):
+        """
+
+        :param pre:
+        :return:
+        """
         self.pre = self.pre + pre
 
     def getPre(self):
+        """
+
+        :return:
+        """
         return self.pre
 
     def getPost(self):
+        """
+
+        :return:
+        """
         return self.post
 
+    def hasPre(self):
+        """Do we have a 'pre' part?
+
+        :return:
+        """
+        return self.pre != ""
+
+    def hasPost(self):
+        """Do we have a 'post' part?
+
+        :return:
+        """
+        return self.post != ""
+
     def hasPrePost(self):
-        return (self.pre != "") and (self.post != "")
+        """
+
+        :return:
+        """
+        return self.hasPre() and self.hasPost()
 
     def handleClassCenter(self):
+        """
+
+        :return:
+        """
         pass
 
     def handleClassFontsize(self, fontsize):
+        """
+
+        :param fontsize:
+        :return:
+        """
         pass
 
     def handleDivAndSpan(self, elem):
+        """
+
+        :param elem:
+        :return:
+        """
         pass
 
 
@@ -112,47 +171,83 @@ class LaTeXDecorator(Decorator):
         "upright": "upshape"}
 
     def handleClassCenter(self):
-        """
-        Add center environment
+        """Add LaTeX center environment.
         """
         self.addPre(self.TEX_CENTER_BEFORE)
         self.addPost(self.TEX_CENTER_AFTER)
 
     def handleClassFontsize(self, fontsize):
         """Add new fontsize.
+
+        :param fontsize:
+        :return:
         """
         self.addPre("{\\" + fontsize + "{}")
         self.addPost("}")
 
     def handleClassFontfamily(self, fontfamily):
         """Add new fontfamily.
+
+        :param fontfamily:
+        :return:
         """
         self.addPre("{\\" + self.TEX_FONTFAMILY_TAG[fontfamily] + "{}")
         self.addPost("}")
 
     def getRawBlock(self, txt):
+        """
+
+        :param txt:
+        :return:
+        """
         return pf.RawBlock(txt, format=self.FORMAT)
 
     def getRawInline(self, txt):
+        """
+
+        :param txt:
+        :return:
+        """
         return pf.RawInline(txt, format=self.FORMAT)
 
     def getBeforeBlock(self):
+        """
+
+        :return:
+        """
         if self.hasPrePost():
             return self.getRawBlock(self.getPre())
 
     def getBeforeInline(self):
+        """
+
+        :return:
+        """
         if self.hasPrePost():
             return self.getRawInline(self.getPre())
 
     def getAfterBlock(self):
+        """
+
+        :return:
+        """
         if self.hasPrePost():
             return self.getRawBlock(self.getPost())
 
     def getAfterInline(self):
+        """
+
+        :return:
+        """
         if self.hasPrePost():
             return self.getRawInline(self.getPost())
 
     def handleClassJustifiedInDiv(self, alignment):
+        """Handle classes with justified (left/right) in DIV blocks.
+
+        :param alignment: "left" or "right"
+        :return: None
+        """
         if alignment == "left":
             self.addPre("\n\\begin{flushright}\n")
             self.addPost("\n\end{flushright}\n")
@@ -163,9 +258,13 @@ class LaTeXDecorator(Decorator):
 
         if alignment == "center":
             self.handleClassCenter()
+        pass
 
     def handleDiv(self, elem):
         """Handle DIV and SPAN Blocks in LaTeX Context.
+
+        :param elem:
+        :return:
         """
         if 'center' in elem.classes:
             self.handleClassCenter()
@@ -176,12 +275,31 @@ class LaTeXDecorator(Decorator):
         if 'justifiedright' in elem.classes:
             self.handleClassJustifiedInDiv("right")
 
+    def handleIndexSpan(self, elem):
+        """
+
+        :param elm:
+        :return:
+        """
+        self.addPre("")
+        self.addPost("\\index{"+elem.attribute["index"]+"}")
+
     def handleSpan(self, elem):
         """Handle DIV and SPAN Blocks in LaTeX Context.
+
+        :param elem:
+        :return:
         """
+        if elem.attribute["index"]:
+            self.handleIndexSpan(elem)
+        else:
+            pass
 
     def handleDivAndSpan(self, elem):
         """Handle DIV and SPAN Blocks in LaTeX Context.
+
+        :param elem:
+        :return:
         """
         for fontsize in self.FONTSIZECLASSES:
             if fontsize in elem.classes:
@@ -200,15 +318,22 @@ class LaTeXDecorator(Decorator):
 
 
 class HTMLDecorator(Decorator):
+    """
+
+    """
     pass
 
 
 def main():
+    """
+
+    :return:
+    """
     pass
 
 
 """
- as always
+And, as always:
 """
 if __name__ == "__main__":
     main()
